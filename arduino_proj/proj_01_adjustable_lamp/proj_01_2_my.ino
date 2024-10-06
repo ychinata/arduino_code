9db6976d545048ec21916da3e43a2619f35c1c05
ychinata <ychinata@qq.com>
2024-10-06 21:33:08 +0800

    No.: proj_1_adjustable_lamp Author:xy Description：重构，拆分dht11.cpp

--- arduino_proj/proj_01_adjustable_lamp/proj_01_2_my.ino
+++ arduino_proj/proj_01_adjustable_lamp/proj_01_2_my.ino
@@ -4,19 +4,12 @@
  *@Date: 2024.2.14
  *********************************************/
 #include <Wire.h>
-#include <DHT.h>  // 安装DHT sensor library和Adafruit Unified Sensor两个库
 #include "bh1750.h"
+#include"dht11.h"
 #include "led_array.h"
 #include "oled_i2c_adafruit.h"
 #include "power_measure.h"
 #include "rotary_encoder.h"
-
-//定义针脚
-#define DHTPIN 8 
-//定义类型，DHT11或者其它
-#define DHTTYPE DHT11
-//进行初始设置 
-DHT dht(DHTPIN, DHTTYPE);
 
 // IO config
 #define LED_PIN 5   // D5
@@ -28,7 +21,7 @@ void setup() {
     BH1750_Init();                  //初始化BH1750
     OLED_I2C_Adafruit_Init();  
     ROTARYENCODER_Init();  
-    dht.begin(); //DHT开始工作
+    DHT11_Init();
 
     // 一路灯板D4
     pinMode(LED_PIN, OUTPUT);
@@ -54,34 +47,7 @@ void loop() {
     DHT11_Getdata(&humidity, &temp);                // 获取温度和温度
     OLED_ShowBright(brightValue, ledPwmValue, lux, humidity, temp);
     //DHT11_Showdata();
-    delay(2000);
-}
-
-/* 获取DHT11的温度和湿度
- * 结果为两位小数：Humidity: 52.00, Temperature: 30.40
- */
-void DHT11_Getdata(float* humidity, float* temp) {
-    if (humidity == nullptr || temp == nullptr) {
-        Serial.print("DHT11_Getdata error! Nullptr.");
-        return;
-    }
-    // 两次检测之间，要等几秒钟，这个传感器有点慢。
-    //delay(2000);
-    // 读温度或湿度要用250毫秒
-    *humidity = dht.readHumidity();//读湿度
-    *temp = dht.readTemperature();//读温度，默认为摄氏度
-}
-
-void DHT11_Showdata() {
-    float humidity = 0.0;
-    float temp = 0.0;
-
-    DHT11_Getdata(&humidity, &temp);
-    Serial.print("Humidity: ");//湿度
-    Serial.println(humidity);
-    Serial.print("Temperature: ");//温度
-    Serial.print(temp);
-    Serial.println(" ℃ ");
+    delay(2000);        // 得改用定时器来实现，要不然无法实时捕获编码器?
 }
 
 void work3() {
